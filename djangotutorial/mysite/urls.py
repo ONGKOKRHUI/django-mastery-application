f7b2b9cce5a27b8424f1ff2c05b8b605e0c2e567
Vincent Ong Kok Rhui <133461913+ONGKOKRHUI@users.noreply.github.com>
2025-12-16 06:55:21 +0800

    basic request and response flow

--- djangotutorial/mysite/urls.py
+++ djangotutorial/mysite/urls.py
@@ -15,8 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('polls/', include('polls.urls')), # include the polls app urls using include()
+    path('admin/', admin.site.urls), # pre built admin interface
 ]

3072e0b444b3c22c047fcc017f8fcb89b7604c36
Vincent Ong Kok Rhui <133461913+ONGKOKRHUI@users.noreply.github.com>
2025-12-19 07:22:29 +0800

    writing views

--- djangotutorial/polls/urls.py
+++ djangotutorial/polls/urls.py
@@ -2,11 +2,18 @@ from django.urls import path
 
 from . import views
 
+#app names are to used to tell django this url is called 
+# from the details view in the polls app html template
+#we will have many apps in the future
+app_name = "polls"
 urlpatterns = [
     # ex: /polls/
     path("", views.index, name="index"),
-    # ex: /polls/5/
+    # ex: /polls/5/      ## the 'name' value as called by the {% url %} template tag
     path("<int:question_id>/", views.detail, name="detail"),
+    # If you want to change the URL of the polls detail view to something else, 
+    # you can change the name attribute of the path() call. like polls/specifics/12/
+    #path("specifics/<int:question_id>/", views.detail, name="detail"),
     # ex: /polls/5/results/
     path("<int:question_id>/results/", views.results, name="results"),
     # ex: /polls/5/vote/
--- djangotutorial/polls/views.py
+++ djangotutorial/polls/views.py
@@ -2,7 +2,12 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
-
+"""
+Each view is responsible for doing one of two
+ things: returning an HttpResponse object containing
+ the content for the requested page, or raising an 
+ exception such as Http404. The rest is up to you.
+"""
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
@@ -19,20 +24,43 @@ def vote(request, question_id):
 
 
 from .models import Question
-from django.template import loader
+#from django.template import loader (can use render shortcut instead)
 
+""" 
+displays the latest 5 poll questions in the system, 
+separated by commas, according to publication date:
+"""
 def index(request):
     latest_question_list = Question.objects.order_by("-pub_date")[:5]
-    template = loader.get_template("polls/index.html")
+    #template = loader.get_template("polls/index.html")
     context = {"latest_question_list": latest_question_list}
     return render(request, "polls/index.html", context)
 
 
 from django.http import Http404
+"""
+The new concept here: The view raises the Http404 
+exception if a question with the requested ID doesn’t exist.
+"""
 
-def detail(request, question_id):
+"""def detail(request, question_id):
     try:
         question = Question.objects.get(pk=question_id)
     except Question.DoesNotExist:
         raise Http404("Question does not exist")
     return render(request, "polls/detail.html", {"question": question})
+"""
+
+"""
+It’s a very common idiom to use get() and raise Http404 if the object doesn’t exist.
+ Django provides a shortcut. Here’s the detail() view, rewritten:
+"""
+from django.shortcuts import get_object_or_404, render
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/detail.html", {"question": question})
+
+"""
+There’s also a get_list_or_404() function, which works just as get_object_or_404() – except using filter() 
+instead of get(). It raises Http404 if the list is empty.
+"""
